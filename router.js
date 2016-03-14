ed51b96375f7b92885e803ee0550dff29c0d652b
lwenke01 <lwenke@gmail.com>
2016-03-14 00:11:02 -0700

    got the post to work, now need to retrieve the data and delete

---
+++ router.js
@@ -0,0 +1,28 @@
+'use strict';
+// var url = require('url');
+var Router = module.exports = function() {
+  this.routes = {
+    'GET': {},
+    'POST': {},
+    'DELETE': {}
+  };
+};
+
+
+Router.prototype.post = function(route, cb){
+  this.routes['POST'][route] = cb;
+};
+Router.prototype.get = function(route, cb){
+  this.routes['GET'][route] = cb;
+};
+Router.prototype.delete = function(route, cb){
+  this.routes['DELETE'][route] = cb;
+};
+
+Router.prototype.route = function() {
+  return(req, res)=> {
+    var routeFunction = this.routes[req.method][req.url];
+    routeFunction(req, res);
+
+  };
+};
--- server.js
+++ server.js
@@ -1,9 +1,66 @@
 'use strict';
-
 var http = require('http');
-var animalsRouter = require(__dirname + '/routes/animal-route').animalsRouter;
+var fs = require('fs');
+var Router = require('./router.js');
+var Animals = new Router();
+var strftime = require('strftime');
+// var mainRouter = require('./server').mainRouter;
+var idCount = 0;
+// var animalType = fs.readFileSync(__dirname + 'animals/' + req.url.subString(7));
 
 
-http.createServer(animalsRouter.route()).listen(3000, () => {
+Animals.post('/animals', (req, res)=> {
+  console.log('/animals POST request was hit');
+
+  // var date = new Date();
+  // var format = '%Y-%m-%d %H:%M';
+  idCount += 1;
+  req.on('data', (data) =>{
+    var stringData = data.toString();
+    console.log('string data:' + stringData);
+    fs.writeFile(__dirname + '/data/animal' + idCount + '.json', data, (err)=>{
+      console.log(err);
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end();
+
+    });
+  });
+});
+
+Animals.get('/animals', (req, res)=>{
+  // fs.readdir(__dirname + '/animals', (err, dataFiles)=>{
+    //if doesnt contain file
+    // if(err || dataFiles.length === 0) {
+    //   console.log(err);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('got it');
+  res.end();
+    // }
+    //if contains files - get the name and content
+  //   dataFiles.forEach((file)=> {
+  //     var content = fs.readFileSync(__dirname + '/animals/' + file, 'utf8');
+  //     console.log(content);
+  //     res.write(content);
+  //
+  //   });
+  //   res.end();
+  // });
+});
+// get animal by id
+Animals.get('/animals/:id', (req, res, parsedURL)=> {
+  var parseNum = parsedURL.pathname.match(/\/animals\/(\d+)/);
+  fs.readFile(__dirname + '/animals/animal' + parseNum + '.json', data, (err) => {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      return res.end('404 Not Found');
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end();
+  });
+});
+
+// Animals.delete('/animals, ')
+
+http.createServer(Animals.route()).listen(3000, () => {
   console.log('server up on 3000');
 });
